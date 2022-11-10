d65cc2fa4feee0c0d06056e1b8c4b5e0d0f8176c
unskytoo <unskytoo@gmail.com>
2022-11-10 11:27:35 +0800

    task optimize

---
+++ Runtime/Base/Async/Task/MainThreadTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LiteQuark.Runtime
+{
+    public sealed class MainThreadTask : ITask
+    {
+        public bool IsEnd { get; private set; }
+        public bool IsExecute { get; private set; }
+        
+        private readonly Action<object> Func_;
+        private readonly object Param_;
+
+        public MainThreadTask(Action<object> func, object param)
+        {
+            IsEnd = false;
+            IsExecute = false;
+            
+            Func_ = func;
+            Param_ = param;
+        }
+        
+        public void Dispose()
+        {
+        }
+
+        public void Execute()
+        {
+            if (IsExecute || IsEnd)
+            {
+                return;
+            }
+
+            IsEnd = true;
+            IsExecute = true;
+            Func_?.Invoke(Param_);
+        }
+    }
+}
