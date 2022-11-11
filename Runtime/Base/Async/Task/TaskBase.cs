01f20aeb8195a0415625e7f53a37a9a1cfcb821d
unskytoo <unskytoo@gmail.com>
2022-11-11 11:49:53 +0800

    task optimize

---
+++ Runtime/Base/Async/Task/TaskBase.cs
@@ -0,0 +1,34 @@
+﻿namespace LiteQuark.Runtime
+{
+    public abstract class TaskBase : ITask
+    {
+        public bool IsEnd { get; private set; }
+        public bool IsExecute { get; private set; }
+
+        protected TaskBase()
+        {
+            IsEnd = false;
+            IsExecute = false;
+        }
+
+        public abstract void Dispose();
+        
+        public void Execute()
+        {
+            if (IsExecute || IsEnd)
+            {
+                return;
+            }
+
+            IsExecute = true;
+            OnExecute();
+        }
+
+        public void Stop()
+        {
+            IsEnd = true;
+        }
+
+        protected abstract void OnExecute();
+    }
+}
