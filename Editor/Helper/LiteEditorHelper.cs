5cb4c0186b95dfc0abd6a23cad4052d61fad9355
unskytoo <unskytoo@gmail.com>
2022-11-16 11:38:00 +0800

    add navigation editor window

---
+++ Editor/Helper/LiteEditorHelper.cs
@@ -0,0 +1,75 @@
+﻿using System.Reflection;
+using LiteQuark.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace LiteQuark.Editor
+{
+#if UNITY_EDITOR
+    public static class LiteEditorHelper
+    {
+        public static void OpenFolder(string path)
+        {
+#if UNITY_EDITOR_WIN
+            Application.OpenURL(path);
+#elif UNITY_STANDALONE_OSX
+            EditorUtility.RevealInFinder(path);
+#endif
+        }
+
+        public static void Ping(string path)
+        {
+            var obj = AssetDatabase.LoadMainAssetAtPath(path);
+            Ping(obj);
+        }
+        
+        public static void Ping(Object obj)
+        {
+            if (obj == null)
+            {
+                return;
+            }
+            
+            
+            if (obj is DefaultAsset)
+            {
+                var browserType = typeof(EditorWindow).Assembly.GetType("ProjectBrowser");
+                if (browserType != null)
+                {
+                    var instance = browserType.GetField("s_LastInteractedProjectBrowser", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static).GetValue(null);
+                    var setFunc = browserType.GetMethod("SetFolderSelection", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+                    var viewMode = (int)browserType.GetField("m_ViewMode", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static).GetValue(instance);
+                    if (viewMode == 0)
+                    {
+                        Selection.activeObject = obj;
+                    }
+                    else if (setFunc != null)
+                    {
+                        setFunc.Invoke(instance, new object[] { new[] { obj.GetInstanceID() }, false });
+                    }
+                }
+            }
+            else
+            {
+                EditorGUIUtility.PingObject(obj);
+            }
+        }
+
+        public static void OpenAsset(string path)
+        {
+            if (PathHelper.PathIsFile(path))
+            {
+                var obj = AssetDatabase.LoadAssetAtPath<Object>(path);
+                if (obj != null)
+                {
+                    AssetDatabase.OpenAsset(obj);
+                }
+            }
+            else
+            {
+                OpenFolder(path);
+            }
+        }
+    }
+}
+#endif
---
+++ Editor/Navigation/NavigationEditorWindow.cs
@@ -0,0 +1,89 @@
+﻿using LiteQuark.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace LiteQuark.Editor
+{
+    public class NavigationEditorWindow : EditorWindow
+    {
+        private const string SubPath = "Navigation/PathList.json";
+        private NavigationData Data_;
+        
+        [MenuItem("Lite/Navigation")]
+        private static void ShowWin()
+        {
+            var win = GetWindow<NavigationEditorWindow>("Navigation");
+            win.Show();
+        }
+
+        private void OnEnable()
+        {
+            var jsonPath = PathHelper.GetLiteQuarkRootPath(SubPath);
+            Data_ = NavigationData.FromJson(jsonPath);
+        }
+
+        private void OnDisable()
+        {
+            var jsonPath = PathHelper.GetLiteQuarkRootPath(SubPath);
+            NavigationData.ToJson(Data_, jsonPath);
+        }
+
+        private void OnGUI()
+        {
+            using (new EditorGUILayout.VerticalScope())
+            {
+                if (GUILayout.Button("PersistentData Path"))
+                {
+                    LiteEditorHelper.OpenFolder(Application.persistentDataPath);
+                }
+
+                if (GUILayout.Button("TemporaryCache Path"))
+                {
+                    LiteEditorHelper.OpenFolder(Application.temporaryCachePath);
+                }
+
+                foreach (var path in Data_.GetPathList())
+                {
+                    using (new EditorGUILayout.HorizontalScope())
+                    {
+                        if (GUILayout.Button(path))
+                        {
+                            LiteEditorHelper.Ping(path);
+                            
+                            if (Event.current.alt)
+                            {
+                                LiteEditorHelper.OpenAsset(path);
+                                Event.current.Use();
+                            }
+                        }
+
+                        if (GUILayout.Button("Delete", GUILayout.ExpandWidth(false)))
+                        {
+                            Data_.RemovePath(path);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            GUILayout.FlexibleSpace();
+
+            EditorGUILayout.LabelField("Click the path to select it, and press and hold alt to open it directly");
+
+            if (mouseOverWindow == this)
+            {
+                if (Event.current.type == EventType.DragUpdated)
+                {
+                    DragAndDrop.visualMode = DragAndDropVisualMode.Generic;
+                }
+                else if (Event.current.type == EventType.DragExited)
+                {
+                    Data_.AddPath(DragAndDrop.paths);
+                    
+                    Focus();
+                    Event.current.Use();
+                }
+            }
+        }
+    }
+}
