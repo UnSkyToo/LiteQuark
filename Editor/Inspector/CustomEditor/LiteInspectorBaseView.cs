b9504cbe368f2844888600c003cc78eb08f1b5cc
unskytoo <unskytoo@gmail.com>
2024-04-24 15:17:13 +0800

    [A] 新增Debugger方便监控System信息

---
+++ Editor/Inspector/CustomEditor/LiteInspectorBaseView.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+
+namespace LiteQuark.Editor
+{
+    internal abstract class LiteInspectorBaseView : UnityEditor.Editor
+    {
+        private bool IsCompiling_ = false;
+
+        public sealed override void OnInspectorGUI()
+        {
+            if (IsCompiling_ && !EditorApplication.isCompiling)
+            {
+                IsCompiling_ = false;
+                OnCompileCompleted();
+            }
+            else if (!IsCompiling_ && EditorApplication.isCompiling)
+            {
+                IsCompiling_ = true;
+                OnCompileStart();
+            }
+            
+            OnDraw();
+            
+            Repaint();
+        }
+
+        protected virtual void OnDraw()
+        {
+            DrawDefaultInspector();
+        }
+
+        protected virtual void OnCompileStart()
+        {
+        }
+
+        protected virtual void OnCompileCompleted()
+        {
+        }
+    }
+}
---
+++ Editor/Inspector/CustomEditor/LiteLauncherInspectorView.cs
@@ -0,0 +1,55 @@
+﻿using LiteQuark.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace LiteQuark.Editor
+{
+    [CustomEditor(typeof(LiteDebugger))]
+    internal class LiteLauncherInspectorView : LiteInspectorBaseView
+    {
+        private bool AssetFoldout_ = false;
+        private bool ObjectPoolFoldout_ = false;
+        
+        protected override void OnDraw()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            DrawFoldout(ref AssetFoldout_, "Asset", DrawAsset);
+            DrawFoldout(ref ObjectPoolFoldout_, "ObjectPool", DrawObjectPool);
+        }
+
+        private void DrawFoldout(ref bool foldout, string label, System.Action func)
+        {
+            foldout = EditorGUILayout.BeginFoldoutHeaderGroup(foldout, label);
+            if (foldout)
+            {
+                func.Invoke();
+            }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+        }
+
+        private void DrawAsset()
+        {
+            var system = LiteRuntime.Asset;
+        }
+
+        private void DrawObjectPool()
+        {
+            var cache = LiteRuntime.ObjectPool.GetPoolCache();
+            if (cache.Count == 0)
+            {
+                EditorGUILayout.LabelField("Empty");
+                return;
+            }
+
+            EditorGUILayout.LabelField("Name", "Count\tActive\tInactive");
+            foreach (var chunk in cache)
+            {
+                EditorGUILayout.LabelField($"{chunk.Value.Name}", $"{chunk.Value.CountAll}\t{chunk.Value.CountActive}\t{chunk.Value.CountInactive}");
+            }
+        }
+    }
+}
