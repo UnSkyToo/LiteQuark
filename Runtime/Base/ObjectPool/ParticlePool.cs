a6a434bad46ffb992c3e589b8caf7a58244f7fe0
unskytoo <unskytoo@gmail.com>
2024-04-24 15:15:51 +0800

    [U] 更新ObjectPoolSystem

---
+++ Runtime/Base/ObjectPool/ParticlePool.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace LiteQuark.Runtime
+{
+    public class ParticlePool : GameObjectPool
+    {
+        public ParticlePool()
+            : base()
+        {
+        }
+        
+        protected override void OnRelease(GameObject go)
+        {
+            var particles = go.GetComponentsInChildren<ParticleSystem>();
+            foreach (var particle in particles)
+            {
+                particle.Stop();
+            }
+            
+            base.OnRelease(go);
+        }
+
+        public override GameObject Alloc(Transform parent)
+        {
+            var go = base.Alloc(parent);
+
+            var particles = go.GetComponentsInChildren<ParticleSystem>();
+            foreach (var particle in particles)
+            {
+                particle.Play();
+            }
+            
+            return go;
+        }
+    }
+}
