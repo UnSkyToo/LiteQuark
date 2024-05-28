a896b6e35220f7e3e2caaffbcf65fcf01db85642
unskytoo <unskytoo@gmail.com>
2024-05-28 15:45:32 +0800

    [A] 新增音效系统

---
+++ Assets/LiteQuark/Runtime/Audio/AudioObject.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+
+namespace LiteQuark.Runtime
+{
+    public class AudioObject : BaseObject
+    {
+        public AudioType Type { get; private set; }
+        public bool IsLoaded { get; private set; }
+        public AudioSource Source { get; private set; }
+        public GameObject Carrier { get; private set; }
+
+        public override string DebugName => $"AudioClip_{Source?.clip?.name}";
+
+        public AudioObject(AudioType type)
+            : base()
+        {
+            Type = type;
+            IsLoaded = false;
+        }
+
+        public void Load(EmptyGameObjectPool pool, Transform parent, AudioClip clip, bool isLoop, float volume, bool isMute)
+        {
+            if (IsLoaded)
+            {
+                return;
+            }
+            
+            Carrier = pool.Alloc(parent);
+            if (Carrier == null)
+            {
+                return;
+            }
+
+            Source = Carrier.GetOrAddComponent<AudioSource>();
+            Source.clip = clip;
+            Source.volume = volume;
+            Source.loop = isLoop;
+            Source.pitch = 1.0f;
+            Source.mute = isMute;
+            Carrier.name = DebugName;
+            IsLoaded = true;
+        }
+
+        public void Unload(EmptyGameObjectPool pool)
+        {
+            if (!IsLoaded)
+            {
+                return;
+            }
+
+            if (Carrier != null)
+            {
+                Source = null;
+                pool.Recycle(Carrier);
+                Carrier = null;
+            }
+
+            IsLoaded = false;
+        }
+
+        public bool IsValid()
+        {
+            return IsLoaded && Source != null;
+        }
+
+        public bool IsEnd()
+        {
+            if (!IsValid())
+            {
+                return false;
+            }
+
+            return !Source.isPlaying;
+        }
+
+        public bool Play()
+        {
+            if (!IsValid())
+            {
+                return false;
+            }
+
+            Source.Play();
+            return true;
+        }
+
+        public bool Mute(bool isMute)
+        {
+            if (!IsValid())
+            {
+                return false;
+            }
+
+            Source.mute = isMute;
+            return true;
+        }
+
+        public bool Stop()
+        {
+            if (!IsValid())
+            {
+                return false;
+            }
+
+            Source.Stop();
+            return true;
+        }
+
+        public bool SetVolume(float volume)
+        {
+            if (!IsValid())
+            {
+                return false;
+            }
+
+            Source.volume = volume;
+            return true;
+        }
+    }
+}
---
+++ Assets/LiteQuark/Runtime/Audio/AudioType.cs
@@ -0,0 +1,8 @@
+﻿namespace LiteQuark.Runtime
+{
+    public enum AudioType : byte
+    {
+        Music = 0,
+        Sound = 1,
+    }
+}
