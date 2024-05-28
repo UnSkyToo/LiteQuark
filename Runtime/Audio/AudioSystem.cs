b8fab8b8aa8f958a733998e891166f6f25b90b05
unskytoo <unskytoo@gmail.com>
2024-05-28 15:45:32 +0800

    [A] 新增音效系统

--- Runtime/Audio/AudioSystem.cs
+++ Runtime/Audio/AudioSystem.cs
@@ -1,40 +1,195 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace LiteQuark.Runtime
 {
-    public sealed class AudioSystem : ISystem
+    public sealed class AudioSystem : ISystem, ITick
     {
-        private AudioSource Source_;
-
+        private bool MuteSound_ = false;
+        private bool MuteMusic_ = false;
+        
+        private readonly Dictionary<ulong, AudioObject> AudioCache_ = new Dictionary<ulong, AudioObject>();
+        private readonly List<AudioObject> RemoveList_ = new List<AudioObject>();
+        private readonly EmptyGameObjectPool Pool_ = null;
+        private Transform Root_ = null;
+        
         public AudioSystem()
         {
+            if (Root_ == null)
+            {
+                Root_ = new GameObject("Audio").transform;
+                Root_.hideFlags = HideFlags.NotEditable;
+                Object.DontDestroyOnLoad(Root_);
+            }
+            
+            Pool_ = LiteRuntime.ObjectPool.GetEmptyGameObjectPool("Audio");
+            
+            AudioCache_.Clear();
+            RemoveList_.Clear();
+            MuteSound_ = false;
+            MuteMusic_ = false;
         }
 
         public void Dispose()
         {
-        }
-
-        private AudioSource GetSource()
-        {
-            if (Source_ == null)
+            foreach (var audio in RemoveList_)
             {
-                Source_ = Object.FindFirstObjectByType<AudioSource>();
+                audio.Stop();
+                audio.Unload(Pool_);
             }
+            RemoveList_.Clear();
 
-            return Source_;
+            foreach (var chunk in AudioCache_)
+            {
+                chunk.Value.Stop();
+                chunk.Value.Unload(Pool_);
+            }
+            AudioCache_.Clear();
+            
+            if (Root_ != null)
+            {
+                Object.DestroyImmediate(Root_.gameObject);
+                Root_ = null;
+            }
         }
         
-        public void PlaySound(string resName)
+        public void Tick(float deltaTime)
         {
-            LiteRuntime.Asset.LoadAssetAsync<AudioClip>(resName, (clip) =>
+            foreach (var chunk in AudioCache_)
+            {
+                if (chunk.Value.IsEnd())
+                {
+                    RemoveList_.Add(chunk.Value);
+                }
+            }
+
+            if (RemoveList_.Count > 0)
+            {
+                foreach (var audio in RemoveList_)
+                {
+                    AudioCache_.Remove(audio.UniqueID);
+                    audio.Unload(Pool_);
+                }
+                RemoveList_.Clear();
+            }
+        }
+        
+        private ulong PlayAudio(AudioType type, Transform parent, string path, bool isLoop = false, float volume = 1.0f)
+        {
+            var audio = new AudioObject(type);
+            AudioCache_.Add(audio.UniqueID, audio);
+
+            LiteRuntime.Asset.LoadAssetAsync<AudioClip>(path, (clip) =>
             {
                 if (clip == null)
                 {
+                    LLog.Warning($"can't play audio : {path}");
+                    RemoveList_.Add(audio);
                     return;
                 }
-                
-                GetSource()?.PlayOneShot(clip);
+
+                audio.Load(Pool_, parent, clip, isLoop, volume, false);
+
+                switch (type)
+                {
+                    case AudioType.Sound:
+                        if (MuteSound_)
+                        {
+                            audio.Mute(MuteSound_);
+                        }
+                        break;
+                    case AudioType.Music:
+                        if (MuteMusic_)
+                        {
+                            audio.Mute(MuteMusic_);
+                        }
+                        break;
+                }
+
+                audio.Play();
             });
+
+            return audio.UniqueID;
+        }
+
+        public ulong PlaySound(string path, bool isLoop = false, float volume = 1.0f)
+        {
+            return PlayAudio(AudioType.Sound, Root_, path, isLoop, volume);
+        }
+
+        public ulong PlayMusic(string path, bool isLoop = true, float volume = 1.0f, bool isOnly = true)
+        {
+            if (isOnly)
+            {
+                StopAllMusic();
+            }
+
+            return PlayAudio(AudioType.Music, Root_, path, isLoop, volume);
+        }
+
+        public void StopAudio(ulong id)
+        {
+            if (AudioCache_.ContainsKey(id))
+            {
+                AudioCache_[id].Stop();
+                RemoveList_.Add(AudioCache_[id]);
+            }
+        }
+
+        public void StopAllSound()
+        {
+            foreach (var chunk in AudioCache_)
+            {
+                if (chunk.Value.Type == AudioType.Sound)
+                {
+                    chunk.Value.Stop();
+                    RemoveList_.Add(chunk.Value);
+                }
+            }
+        }
+
+        public void StopAllMusic()
+        {
+            foreach (var chunk in AudioCache_)
+            {
+                if (chunk.Value.Type == AudioType.Music)
+                {
+                    chunk.Value.Stop();
+                    RemoveList_.Add(chunk.Value);
+                }
+            }
+        }
+
+        public void MuteAllAudio(bool isMute)
+        {
+            MuteAllSound(isMute);
+            MuteAllMusic(isMute);
+        }
+
+        public void MuteAllSound(bool isMute)
+        {
+            MuteSound_ = isMute;
+
+            foreach(var chunk in AudioCache_)
+            {
+                if (chunk.Value.Type == AudioType.Sound)
+                {
+                    chunk.Value.Mute(isMute);
+                }
+            }
+        }
+
+        public void MuteAllMusic(bool isMute)
+        {
+            MuteMusic_ = isMute;
+
+            foreach (var chunk in AudioCache_)
+            {
+                if (chunk.Value.Type == AudioType.Music)
+                {
+                    chunk.Value.Mute(isMute);
+                }
+            }
         }
     }
 }
