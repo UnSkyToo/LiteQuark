aa9d2a95dde8be5ecdc1aebe4bf376373d30d7fa
unskytoo <unskytoo@gmail.com>
2024-07-15 15:21:14 +0800

    [A] 增加外部打包逻辑接口（Jenkins等CI系统)

---
+++ Editor/Builder/ProjectBuilderCommandLine.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+namespace LiteQuark.Editor
+{
+    public static class ProjectBuilderCommandLine
+    {
+        public static bool BuildProject(BuildTarget target, ResBuildConfig resCfg, AppBuildConfig appCfg)
+        {
+            var buildCfg = new ProjectBuildConfig(target, resCfg, appCfg);
+            var result = new ProjectBuilder().Build(buildCfg);
+            return result.IsSuccess;
+        }
+    }
+}
