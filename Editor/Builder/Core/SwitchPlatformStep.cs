a28ccc9a5118ea5ccbe411601cf6cc6ec043d7d5
unskytoo <unskytoo@gmail.com>
2024-07-17 13:55:46 +0800

    [U] 拆分switch platform到step中

---
+++ Editor/Builder/Core/SwitchPlatformStep.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+
+namespace LiteQuark.Editor
+{
+    /// <summary>
+    /// Switch platform to target
+    /// </summary>
+    internal sealed class SwitchPlatformStep : IBuildStep
+    {
+        public string Name => "Switch Platform Step";
+
+        public void Execute(ProjectBuilder builder)
+        {
+            if (EditorUserBuildSettings.activeBuildTarget != builder.Target)
+            {
+                var targetGroup = BuildPipeline.GetBuildTargetGroup(builder.Target);
+                EditorUserBuildSettings.SwitchActiveBuildTarget(targetGroup, builder.Target);
+                AssetDatabase.Refresh();
+            }
+        }
+    }
+}
