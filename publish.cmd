bfc6f264a9637961c0b7ada292616a8c36d8aa0d
unskytoo <unskytoo@gmail.com>
2024-07-19 15:01:25 +0800

    [U] 更新publish命令，解决ver读取问题

--- publish.cmd
+++ publish.cmd
@@ -1,6 +1,6 @@
 @echo off
 set /p ver=version:
 git subtree split --prefix=Assets/LiteQuark --branch LiteQuark
-git tag ver LiteQuark
+git tag %ver% LiteQuark
 git push origin LiteQuark --tags
 pause
