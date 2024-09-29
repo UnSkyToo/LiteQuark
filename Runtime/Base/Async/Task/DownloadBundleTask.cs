0c5c0b8427d37528066a1196adfb5b87a0e47d79
unskytoo <unskytoo@gmail.com>
2024-09-29 17:46:13 +0800

    [A] 新增DownloadBundleTask

---
+++ Runtime/Base/Async/Task/DownloadBundleTask.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace LiteQuark.Runtime
+{
+    public class DownloadBundleTask : BaseTask
+    {
+        private readonly string BundleUri_;
+        private Action<AssetBundle> Callback_;
+        private UnityWebRequest Request_;
+        
+        public DownloadBundleTask(string bundleUri, Action<AssetBundle> callback)
+            : base()
+        {
+            BundleUri_ = bundleUri;
+            Callback_ = callback;
+        }
+
+        public override void Dispose()
+        {
+            Callback_ = null;
+        }
+
+        protected override void OnExecute()
+        {
+            LiteRuntime.Task.MonoBehaviourInstance.StartCoroutine(ExecuteInternal());
+        }
+
+        private IEnumerator ExecuteInternal()
+        {
+            Request_ = UnityWebRequestAssetBundle.GetAssetBundle(BundleUri_);
+            var asyncOperation = Request_.SendWebRequest();
+            yield return asyncOperation;
+
+            if (Request_.result != UnityWebRequest.Result.Success)
+            {
+                LLog.Error($"Failed to download bundle : {BundleUri_}");
+                Callback_?.Invoke(null);
+            }
+            else
+            {
+                var bundle = DownloadHandlerAssetBundle.GetContent(Request_);
+                Callback_?.Invoke(bundle);
+            }
+
+            Stop();
+        }
+    }
+}
