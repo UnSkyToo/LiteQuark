7768fd85254bc2bb0644c1ca1a9953bd5908c294
unskytoo <unskytoo@gmail.com>
2024-11-14 16:58:32 +0800

    [U] 排序属性对Property和Filed生效

--- Runtime/Base/Attribute/LitePriorityAttribute.cs
+++ Runtime/Base/Attribute/LitePriorityAttribute.cs
@@ -3,6 +3,10 @@ using System.Diagnostics;
 
 namespace LiteQuark.Runtime
 {
+    /// <summary>
+    /// 编辑器绘制属性优先级，越大越先绘制
+    /// <para>可用于排序object中的propery、filed或者OptionalType中的type（针对class）</para>
+    /// </summary>
     [Conditional("UNITY_EDITOR")]
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
     public sealed class LitePriorityAttribute : Attribute
