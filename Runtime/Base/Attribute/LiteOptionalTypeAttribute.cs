718c8cf07cffb8d5a6b72b10d0ecfa616f8c415d
unskytoo <unskytoo@gmail.com>
2024-11-14 11:50:59 +0800

    [U] 补充编辑器OptionType的参数说明

--- Runtime/Base/Attribute/LiteOptionalTypeAttribute.cs
+++ Runtime/Base/Attribute/LiteOptionalTypeAttribute.cs
@@ -3,6 +3,13 @@ using System.Diagnostics;
 
 namespace LiteQuark.Runtime
 {
+    /// <summary>
+    /// 配合LitePropertyType.OptionalType使用
+    /// <para>配置可选类LiteOptionalType(typeof(IData), "数据")来声明类型</para>
+    /// 注意：需要继承<b>IHasData</b>来决定编辑器是否数据内容
+    /// <param name="BaseType">基类类型（推荐用接口）</param>
+    /// <param name="DataTitle">数据字段的标题</param>
+    /// </summary>
     [Conditional("UNITY_EDITOR")]
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
     public class LiteOptionalTypeAttribute : Attribute
@@ -10,6 +17,8 @@ namespace LiteQuark.Runtime
         public Type BaseType { get; }
         public string DataTitle { get; }
         
+        /// <param name="baseType">基类类型（推荐用接口）</param>
+        /// <param name="dataTitle">数据字段的标题</param>
         public LiteOptionalTypeAttribute(Type baseType, string dataTitle)
         {
             BaseType = baseType;
@@ -17,6 +26,15 @@ namespace LiteQuark.Runtime
         }
     }
     
+    /// <summary>
+    /// 配合LitePropertyType.OptionalType使用
+    /// <para>配置可选类LiteOptionalType(typeof(IData), "数据")来声明类型</para>
+    /// 注意：需要继承<b>IHasData</b>来决定编辑器是否数据内容
+    /// <param name="BaseType">基类类型（推荐用接口）</param>
+    /// <param name="DefaultType">添加数据时，默认实例的子类类型</param>
+    /// <param name="ElementTitle">每个元素的标题开头</param>
+    /// <param name="DataTitle">数据字段的标题</param>
+    /// </summary>
     [Conditional("UNITY_EDITOR")]
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
     public class LiteOptionalTypeListAttribute : Attribute
@@ -26,6 +44,10 @@ namespace LiteQuark.Runtime
         public string ElementTitle { get; }
         public string DataTitle { get; }
         
+        /// <param name="baseType">基类类型（推荐用接口）</param>
+        /// <param name="defaultType">添加数据时，默认实例的子类类型</param>
+        /// <param name="elementTitle">每个元素的标题开头</param>
+        /// <param name="dataTitle">数据字段的标题</param>
         public LiteOptionalTypeListAttribute(Type baseType, Type defaultType, string elementTitle, string dataTitle)
         {
             BaseType = baseType;
