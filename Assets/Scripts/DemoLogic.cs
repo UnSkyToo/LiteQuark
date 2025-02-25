a1597cc8eb0e3286c8702fb4477386bb8a9e6765
unskytoo <unskytoo@gmail.com>
2025-02-25 17:04:44 +0800

    [U] 准备开始demo工程

---
+++ Assets/Scripts/DemoLogic.cs
@@ -0,0 +1,24 @@
+﻿using LiteQuark.Runtime;
+
+namespace LiteQuark.Demo
+{
+    public class DemoLogic : ILogic
+    {
+        public DemoLogic()
+        {
+        }
+        
+        public bool Startup()
+        {
+            return true;
+        }
+
+        public void Shutdown()
+        {
+        }
+        
+        public void Tick(float deltaTime)
+        {
+        }
+    }
+}
