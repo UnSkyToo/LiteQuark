c4fa775c3f24cea891220fbd29064ac9478a6188
unskytoo <unskytoo@gmail.com>
2025-03-06 11:08:15 +0800

    [U] 放弃System代码注册的方式，改用面板管理，和Logic保持一致

---
+++ Runtime/Base/LiteSystemEntryData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace LiteQuark.Runtime
+{
+    [Serializable]
+    public class LiteSystemEntryData
+    {
+        [SerializeField]
+        public bool Disabled;
+        [SerializeField]
+        public string AssemblyQualifiedName;
+
+        public LiteSystemEntryData()
+        {
+        }
+    }
+}
