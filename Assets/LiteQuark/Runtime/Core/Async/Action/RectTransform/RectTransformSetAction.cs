d78ab8e65a1ecca0e9254419ab0a2796a6625ec3
unskytoo <unskytoo@gmail.com>
2025-04-17 13:34:48 +0800

    [A] 新增RectTransformSetAction

---
+++ Assets/LiteQuark/Runtime/Core/Async/Action/RectTransform/RectTransformSetAction.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace LiteQuark.Runtime
+{
+    public class RectTransformSetPositionAction : RectTransformBaseAction
+    {
+        public override string DebugName => $"<RectTransformSetPosition>({RT_.name},{Position_})";
+        
+        private readonly Vector2 Position_;
+
+        public RectTransformSetPositionAction(RectTransform transform, Vector2 position)
+            : base(transform)
+        {
+            Position_ = position;
+        }
+
+        public override void Execute()
+        {
+            if (!CheckSafety())
+            {
+                return;
+            }
+
+            RT_.anchoredPosition = Position_;
+            IsEnd = true;
+        }
+    }
+
+    public static partial class ActionBuilderExtend
+    {
+        public static ActionBuilder RectTransformSetPosition(this ActionBuilder builder, RectTransform transform, Vector2 position)
+        {
+            builder.Add(new RectTransformSetPositionAction(transform, position));
+            return builder;
+        }
+    }
+}
