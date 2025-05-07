d75d12463baf9b867be4c9c4769755d077e003b6
unskytoo <unskytoo@gmail.com>
2025-05-07 14:59:41 +0800

    [U] IDGenerator改为基于时间戳和实例数量的伪唯一方案，而不是递增+1

--- Runtime/Core/Base/IDGenerator.cs
+++ Runtime/Core/Base/IDGenerator.cs
@@ -1,12 +1,40 @@
-﻿namespace LiteQuark.Runtime
+﻿using System;
+
+namespace LiteQuark.Runtime
 {
     public static class IDGenerator
     {
-        private static ulong ID_ = 1;
+        private const int TimestampBits = 50;
+        private const int CounterBits = 14;
 
+        private const int MaxCounter = 1 << CounterBits;
+        
+        private static readonly object Lock_ = new object();
+        private static long LastTimestamp_;
+        private static int Counter_;
+        
         public static ulong NextID()
         {
-            return ID_++;
+            lock (Lock_)
+            {
+                var currentTimestamp = (long)(DateTime.UtcNow - DateTime.UnixEpoch).TotalSeconds;
+                if (currentTimestamp > LastTimestamp_)
+                {
+                    LastTimestamp_ = currentTimestamp;
+                    Counter_ = 1;
+                }
+                else
+                {
+                    Counter_++;
+                    if (Counter_ >= MaxCounter)
+                    {
+                        Counter_ = 1;
+                        LastTimestamp_++;
+                    }
+                }
+
+                return ((ulong)LastTimestamp_ << CounterBits) | (uint)Counter_;
+            }
         }
     }
 }
