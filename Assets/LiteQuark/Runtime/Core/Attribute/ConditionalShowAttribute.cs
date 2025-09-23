c8c36a342701b248ab35486b947bff354f67757d
unskytoo <unskytoo@gmail.com>
2025-09-23 14:56:46 +0800

    [U] ConditionalHide -> ConditionalShow，更准确的名字

--- Assets/LiteQuark/Runtime/Core/Attribute/ConditionalShowAttribute.cs
+++ Assets/LiteQuark/Runtime/Core/Attribute/ConditionalShowAttribute.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 namespace LiteQuark.Runtime
 {
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Class | AttributeTargets.Struct, Inherited = true, AllowMultiple = false)]
-    public sealed class ConditionalHideAttribute : PropertyAttribute
+    public sealed class ConditionalShowAttribute : PropertyAttribute
     {
         public readonly string[] SourceFields = new string[] { };
         public readonly object[] Values = new object[] { };
         public readonly bool[] Inverses = new bool[] { };
         
-        public ConditionalHideAttribute(string sourceField, object value)
+        public ConditionalShowAttribute(string sourceField, object value)
             : this(sourceField, value, false)
         {
         }
         
-        public ConditionalHideAttribute(string sourceField1, object value1, string sourceField2, object value2)
+        public ConditionalShowAttribute(string sourceField1, object value1, string sourceField2, object value2)
             : this(new[] { sourceField1, sourceField2 }, new[] { value1, value2 }, new[] { false, false })
         {
         }
 
-        public ConditionalHideAttribute(string sourceField, object value, bool inverse)
+        public ConditionalShowAttribute(string sourceField, object value, bool inverse)
             : this(new[] { sourceField }, new[] { value }, new[] { inverse })
         {
         }
 
-        public ConditionalHideAttribute(string[] conditionalSourceFields, object[] values, bool[] inverses)
+        public ConditionalShowAttribute(string[] conditionalSourceFields, object[] values, bool[] inverses)
         {
             this.SourceFields = conditionalSourceFields;
             this.Values = values;
