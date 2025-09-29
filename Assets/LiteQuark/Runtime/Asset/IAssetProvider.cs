9d4dc8a24950890ddd488912a4a4323ca51d551b
unskytoo <unskytoo@gmail.com>
2025-09-29 18:14:35 +0800

    [U] 剔除Scene加载多余的内容

--- Assets/LiteQuark/Runtime/Asset/IAssetProvider.cs
+++ Assets/LiteQuark/Runtime/Asset/IAssetProvider.cs
@@ -14,11 +14,11 @@ namespace LiteQuark.Runtime
         void LoadAssetAsync<T>(string assetPath, Action<T> callback) where T : UnityEngine.Object;
         void InstantiateAsync(string assetPath, UnityEngine.Transform parent, Action<UnityEngine.GameObject> callback);
         void InstantiateAsync(string assetPath, UnityEngine.Transform parent, UnityEngine.Vector3 position, UnityEngine.Quaternion rotation, Action<UnityEngine.GameObject> callback);
-        void LoadSceneAsync(string scenePath, string sceneName, UnityEngine.SceneManagement.LoadSceneParameters parameters, Action<bool> callback);
+        void LoadSceneAsync(string scenePath, UnityEngine.SceneManagement.LoadSceneParameters parameters, Action<bool> callback);
         
         void UnloadAsset(string assetPath);
         void UnloadAsset<T>(T asset) where T : UnityEngine.Object;
-        void UnloadSceneAsync(string scenePath, string sceneName, Action callback);
+        void UnloadSceneAsync(string scenePath, Action callback);
         void UnloadUnusedAssets(int maxDepth);
     }
 }
