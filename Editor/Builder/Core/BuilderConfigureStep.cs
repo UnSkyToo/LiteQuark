d91b139b472470e96be439b043c406db406aa677
unskytoo <unskytoo@gmail.com>
2026-01-15 15:16:48 +0800

    [U] 部分打包脚本设置前置，防止没有App编译阶段后跳过这些必须的设置

---
+++ Editor/Builder/Core/BuilderConfigureStep.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+
+namespace LiteQuark.Editor
+{
+    /// <summary>
+    /// Configure builder
+    /// </summary>
+    internal sealed class BuilderConfigureStep : IBuildStep
+    {
+        public string Name => "Builder Configure Step";
+
+        public void Execute(ProjectBuilder builder)
+        {
+            if (EditorUserBuildSettings.activeBuildTarget != builder.Target)
+            {
+                var targetGroup = BuildPipeline.GetBuildTargetGroup(builder.Target);
+                EditorUserBuildSettings.SwitchActiveBuildTarget(targetGroup, builder.Target);
+                AssetDatabase.Refresh();
+            }
+            
+            var namedBuildTarget = LiteEditorUtils.GetNamedBuildTarget(builder.Target);
+            PlayerSettings.SetScriptingBackend(namedBuildTarget, builder.AppConfig.Backend);
+            PlayerSettings.SplashScreen.showUnityLogo = false;
+            
+            EditorUserBuildSettings.development = builder.AppConfig.IsDevelopmentBuild;
+            PlayerSettings.applicationIdentifier = builder.AppConfig.Identifier;
+            PlayerSettings.bundleVersion = builder.Version;
+            PlayerSettings.productName = builder.AppConfig.ProduceName;
+            
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+    }
+}
