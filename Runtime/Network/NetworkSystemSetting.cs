cf75550c30c67231e5fe9ec2397d37b4f2cb1e73
unskytoo <unskytoo@gmail.com>
2026-02-03 18:38:04 +0800

    [U] 重构设置模块，提供System自定义设置的功能和编辑器支持

---
+++ Runtime/Network/NetworkSystemSetting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace LiteQuark.Runtime
+{
+    [Serializable, LiteLabel("网络设置")]
+    public class NetworkSystemSetting : ISystemSetting
+    {
+        [Tooltip("最大并发请求数，避免同时发送过多请求造成服务器压力"), Range(1, 20), SerializeField]
+        public int MaxConcurrentRequests = 5;
+
+        [Tooltip("默认超时时间（秒）"), Range(5, 60), SerializeField]
+        public int DefaultTimeout = 10;
+
+        [Tooltip("启用自动重试机制"), SerializeField]
+        public bool EnableAutoRetry = true;
+
+        [Tooltip("默认重试次数"), ConditionalShow(nameof(EnableAutoRetry), true), Range(1, 10), SerializeField]
+        public int DefaultRetryCount = 3;
+
+        public NetworkSystemSetting()
+        {
+        }
+    }
+}
---
+++ Runtime/UI/UISystemSetting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LiteQuark.Runtime
+{
+    [Serializable, LiteLabel("界面设置")]
+    public class UISystemSetting : ISystemSetting
+    {
+        [SerializeField] public Camera UICamera;
+        [SerializeField] public CanvasScaler.ScaleMode ScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        [SerializeField] public CanvasScaler.ScreenMatchMode MatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
+        [SerializeField] [Range(0, 1)] public float MatchValue = 0f;
+        [SerializeField] public int ResolutionWidth = 1920;
+        [SerializeField] public int ResolutionHeight = 1080;
+
+        public UISystemSetting()
+        {
+        }
+    }
+}
